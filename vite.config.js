17527d07cb24052a47aa268ac84e2b2d9c720233
sidharthgk <sidharthgk2003@gmail.com>
2024-11-24 01:31:33 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Cross-Language-Learning/' // Ensure this matches your repo name
-});
+export default {
+  build: {
+    rollupOptions: {
+      external: [
+        '/Cross-Language-Learning/assets/index-DDIBel0c.js'
+      ]
+    }
+  }
+}
